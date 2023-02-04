d4b1b5c876c2a1977128b3e4e4aea9028fbc4df6
isaacBakugan <97451474+OlivaresIsaac@users.noreply.github.com>
2023-02-03 20:50:09 -0400

    add js input and list to html

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-function printEpico(){
-    alert("hola")
+
+const mainInput = document.getElementById("mainInput")
+const mainButton = document.getElementById("mainButton")
+const results = document.getElementById("results");
+
+mainButton.addEventListener("click", addSong);
+let songs=[];
+let counter=0;
+
+function addSong(){
+    let input = mainInput.value
+    if (input != null && input != '') {
+        let newHtml = buildNewSong(input)
+        results.innerHTML += newHtml
+    
+        mainInput.value=null;
+    }
 }
 
+
+function buildNewSong(songName){
+    songName[counter] = songName
+    counter += 1
+    let newHtmlTag = `<p>${counter}.    ${songName}</p>`
+    return newHtmlTag
+}
